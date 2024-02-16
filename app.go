a522f164a495cc56043fd0bf6f4c5aac1d64d4e2
rudra-shiva <atulsingh834@gmail.com>
2024-02-16 17:02:44 +0530

    intial level complete

--- app.go
+++ app.go
@@ -7,9 +7,19 @@ import (
 
 func main() {
 	fmt.Print("Hello World\n")
-	var investmentValue float64 = 10000
-	years := 10
-	expectedReturnRate := 5.5
-	futurevalue := investmentValue * math.Pow(1+expectedReturnRate/100, float64(years))
+	const inflamationRate float64 = 2.5
+	var investmentValue float64
+	var years float64
+	var expectedReturnRate float64
+	fmt.Print("Investment Value :")
+	fmt.Scan(&investmentValue)
+
+	fmt.Print("Years :")
+	fmt.Scan(&years)
+	fmt.Print("Expected Return Rate :")
+	fmt.Scan(&expectedReturnRate)
+	futurevalue := investmentValue * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futurevalue / math.Pow(1+inflamationRate/100, years)
 	fmt.Println(futurevalue)
+	fmt.Println(futureRealValue)
 }

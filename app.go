df91b05decd8964e094f2ea5636d513e55b5fef8
rudra-shiva <atulsingh834@gmail.com>
2024-02-22 17:01:19 +0530

    pointers completed

--- app.go
+++ app.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	fmt.Print("Hello World\n")
-	const inflamationRate float64 = 2.5
-	var investmentValue float64
-	var years float64
-	var expectedReturnRate float64
-	fmt.Print("Investment Value :")
-	fmt.Scan(&investmentValue)
+	fmt.Println("Pointers Program in Go") // Output: 8
+	age := 32
+	agePointer := &age
+	fmt.Print("Age: ", age, "\n") // Output: Age: 32
+	adultYears := getAdultYears(*agePointer)
+	fmt.Println("Adult age: ", adultYears)
+	adultYear := getAdultYear(agePointer)
+	fmt.Println("Adult age: ", adultYear)
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age) // Output: 0
+}
 
-	fmt.Print("Years :")
-	fmt.Scan(&years)
-	fmt.Print("Expected Return Rate :")
-	fmt.Scan(&expectedReturnRate)
-	futurevalue := investmentValue * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futurevalue / math.Pow(1+inflamationRate/100, years)
-	fmt.Println(futurevalue)
-	fmt.Println(futureRealValue)
+func getAdultYears(age int) int {
+	return age - 18
+}
+
+// passing pointer for calcuating address
+func getAdultYear(age *int) int {
+	return *age - 18
+}
+
+func editAgeToAdultYears(age *int) {
+	*age = *age - 18
 }
